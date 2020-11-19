78534e7e93bf720fe140e6b2ba4cca48340332ec
fengbw <fengbwseu@163.com>
2020-11-19 11:20:07 -0500

    Initial commit

---
+++ src/main/java/com/derek/ProductController.java
@@ -0,0 +1,52 @@
+package com.derek;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+public class ProductController {
+    @Autowired
+    private ProductService service;
+    private Integer id;
+
+    @GetMapping("/products")
+    public List<Product> list(){
+        return service.listAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> get(@PathVariable Integer id){
+        try {
+            Product p = service.get(id);
+            return new ResponseEntity<Product>(p, HttpStatus.OK);
+        } catch (NoSuchElementException e){
+            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/products")
+    public void add(@RequestBody Product product){
+        service.save(product);
+    }
+
+    @PostMapping("/products/{id}")
+    public ResponseEntity<?> update(@RequestBody Product product, @PathVariable Integer id){
+        try{
+            Product existProduct = service.get(id);
+            service.save(product);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/products/{id}")
+    public void delete(@PathVariable Integer id){
+        service.delete(id);
+    }
+}
